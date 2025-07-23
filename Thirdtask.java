037dbbf07acdfab3c570a4019934e5b13b259b4c
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-24 00:35:19 +0530

    3rd task

---
+++ Thirdtask.java
@@ -0,0 +1,9 @@
+public class Thirdtask {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   **");
+        System.out.println("  ***");
+        System.out.println(" ****");
+        System.out.println("*****");
+    }
+}
