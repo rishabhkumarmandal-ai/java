9922ebebc593af354217d8114508dee02160e13f
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-24 00:23:09 +0530

    learning day 2

--- Secondtask.java
+++ Secondtask.java
@@ -1,3 +1,10 @@
 public class Secondtask {
-    
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+
+    }
 }
