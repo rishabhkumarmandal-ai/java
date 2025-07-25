d6944a68a440adaf8b19df60374d27e47800cf10
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-25 20:00:55 +0530

    varaible learning

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -6,6 +6,24 @@ public class Variables {
         int yourNumber;  // 2nd type of declaring variable type
         yourNumber= 45;
         System.out.println(yourNumber);
+        yourNumber =55; // reassigning value to variable
+        System.out.println(yourNumber);
+
+
+        boolean isGood = true;   //boolean varaible
+        boolean isBad = false;
+        System.out.println(isBad);
+
+        float myFloat = 5;  // float varaible that is always comes with decimal point
+        System.out.println(myFloat);
+        double myDouble = 5.9889764; // double vairable that is more precise than float
+        System.out.println(myDouble);
+
+
+        String myString = " My name is Rishabh"; // string varaible
+        System.out.println(myString);
+
+
 
     }
 }
