26746cd76cbb5228bae41079a42226f7a6069f83
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-25 19:45:07 +0530

    4rth day

---
+++ Variables/Variables.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber= 35; // 1st type of declaring variable type
+        int yourNumber;  // 2nd type of declaring variable type
+        yourNumber= 45;
+        System.out.println(yourNumber);
+
+    }
+}
