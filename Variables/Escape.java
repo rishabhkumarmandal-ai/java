26bfcd0c48d8b2fd0f8fd4f44650a6c1ea9d6e4b
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-28 18:21:04 +0530

    5th day learning

---
+++ Variables/Escape.java
@@ -0,0 +1,8 @@
+package Variables;
+
+public class Escape {
+    public static void main (String[] args) {
+        String mystring= " Hello \"Rishabh\" Coding.... "; // string varaible withe escape chaters
+        System.out.println(mystring);
+    }
+}
