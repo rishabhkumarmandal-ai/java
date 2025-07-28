88f8add0947f76df6b636aa145601711697b2dff
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-28 18:38:50 +0530

    challenges done

---
+++ Challenges.java
@@ -0,0 +1,9 @@
+// cahllenge for  left increaing star pattern
+
+public class Challenges {
+    public static void main(String[]args) {
+        System.out.println("\n*\n**\n***\n****\n");
+        System.out.println("\n****\n**\n**\n*\n");
+        System.out.println("\n    *\n  **\n ***\n****\n");
+    }
+}
