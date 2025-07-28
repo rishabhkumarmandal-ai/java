0beb4fffc14650395af0518053b03b83d415dcc6
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-28 18:28:00 +0530

    continue

--- Variables/Escape.java
+++ Variables/Escape.java
@@ -4,5 +4,8 @@ public class Escape {
     public static void main (String[] args) {
         String mystring= " Hello \"Rishabh\" Coding.... "; // string varaible withe escape chaters
         System.out.println(mystring);
+        String mystring2= "Hello \nRishabh coding......"; //string  variable with new line
+        System.out.println(mystring2);
     }
 }
+
