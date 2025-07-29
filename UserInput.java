eb1ae64e8770139eca5442e3b4342b9b105cf2a8
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-07-29 22:08:05 +0530

    6th day

--- UserInput.java
+++ UserInput.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args) {
-        Scanner 
+        Scanner input = new Scanner(System.in); //object declaration 
+        System.out.println(" Your name Please" ); // enter input from user
+        String name= input.nextLine();//taking input from user
+        System.out.println("Good Morning " + name);    //  using input from user   
+        input.close();
     }
 
 }
-    
+
 
