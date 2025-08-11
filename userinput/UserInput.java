bb4f3138561f3d822c8dd34fc6a9b79c59d3060a
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-08-12 04:27:24 +0530

    changed

--- userinput/UserInput.java
+++ userinput/UserInput.java
@@ -7,9 +7,10 @@ public class UserInput {
         System.out.println(" Your name Please" ); // enter input from user
         String name= input.nextLine();//taking input from user
         System.out.println("Good Morning " + name);    //  using input from user   
-        input.close();
+        System.out.println(name + ", also tell me your age");
+        int age = input.nextInt();
+        System.out.println("Your age is " + age + "years old");
+
     }
 
 }
-
-
