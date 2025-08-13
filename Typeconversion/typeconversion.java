18f93d43fe4d6026c334a33dc7bba2b571a97f49
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-08-13 13:59:48 +0530

    change

---
+++ Typeconversion/typeconversion.java
@@ -0,0 +1,13 @@
+package Typeconversion;
+
+public class typeconversion {
+    public static void main(String[] args) {
+        float myFloat = 5; // impcit type conversion
+        System.out.println(myFloat); 
+        int myInt = (int) 5.45f; //explicit type conversion bracket dalna jaruri he aage
+        System.out.println(myInt); ;
+        
+
+
+    }
+}
