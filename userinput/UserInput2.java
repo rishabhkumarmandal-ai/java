5325a5588abab98f1f3b54e5d9fec49ac18f2263
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-08-14 14:58:37 +0530

    update

--- userinput/UserInput2.java
+++ userinput/UserInput2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UserInput2 {
     public static void main(String args[]){
         Scanner inputScanner = new Scanner(System.in);
-        System.out.println("Enter youur age: ");
+        System.out.println("Enter your age: ");
         int age = inputScanner.nextInt();
         System.out.println("Your age is:" + age);
         System.out.println("Enter your height in cm:");
