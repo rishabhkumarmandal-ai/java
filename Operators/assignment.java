f6260cfc440f217476eb754b1901fc969c1260ef
Rishabh kumar Mandal <rishabh@aicentre.org>
2025-08-15 11:24:16 +0530

    json edit

--- Operators/assignment.java
+++ Operators/assignment.java
@@ -2,10 +2,16 @@ package Operators;
 
 public class assignment {
     public static void main(String[] args) {
-        int  myInt = 5; 
-        int myInt2 = 20;
-        myInt += 10; 
-        
+        int num = 10;
+
+        num += 5; // 15
+        num -= 2; // 13
+        num *= 2; // 26
+        num /= 4; // 6
+        num %= 5; // 1
+
+        System.out.println(num);
+
 
     }
 }
